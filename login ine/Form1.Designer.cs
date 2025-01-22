d3bf657b268f9a389994650c2498a564af0b42ca
BUCIO <BUCIO@BUCIO-PC>
2025-01-22 12:28:11 -0600

    Agregar archivos de proyecto.

---
+++ login ine/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace login_ine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtadmin = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.panelFondo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.panelFondo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(448, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña:";
+            // 
+            // txtadmin
+            // 
+            this.txtadmin.Location = new System.Drawing.Point(547, 71);
+            this.txtadmin.Margin = new System.Windows.Forms.Padding(4);
+            this.txtadmin.Name = "txtadmin";
+            this.txtadmin.Size = new System.Drawing.Size(132, 22);
+            this.txtadmin.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(512, 172);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ingresar:";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(547, 118);
+            this.txtContraseña.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(132, 22);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // panelFondo
+            // 
+            this.panelFondo.BackColor = System.Drawing.Color.White;
+            this.panelFondo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelFondo.Controls.Add(this.lblTitulo);
+            this.panelFondo.Controls.Add(this.lblUsuario);
+            this.panelFondo.Controls.Add(this.txtUsuario);
+            this.panelFondo.Controls.Add(this.lblContrasena);
+            this.panelFondo.Controls.Add(this.txtContrasena);
+            this.panelFondo.Controls.Add(this.btnLogin);
+            this.panelFondo.Location = new System.Drawing.Point(12, 12);
+            this.panelFondo.Name = "panelFondo";
+            this.panelFondo.Size = new System.Drawing.Size(358, 228);
+            this.panelFondo.TabIndex = 5;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.lblTitulo.Location = new System.Drawing.Point(6, 16);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(309, 32);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Sistema de Inventario INE";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblUsuario.Location = new System.Drawing.Point(3, 63);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(100, 23);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtUsuario.Location = new System.Drawing.Point(132, 60);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(200, 30);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblContrasena.Location = new System.Drawing.Point(3, 100);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(108, 23);
+            this.lblContrasena.TabIndex = 3;
+            this.lblContrasena.Text = "Contraseña:";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtContrasena.Location = new System.Drawing.Point(132, 100);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '•';
+            this.txtContrasena.Size = new System.Drawing.Size(200, 30);
+            this.txtContrasena.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(67, 160);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 35);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Iniciar Sesión";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtadmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelFondo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Inicio de sesión - INE";
+            this.panelFondo.ResumeLayout(false);
+            this.panelFondo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtadmin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private Panel panelFondo;
+        private Label lblTitulo;
+        private Label lblUsuario;
+        private TextBox txtUsuario;
+        private Label lblContrasena;
+        private TextBox txtContrasena;
+        private Button btnLogin;
+    }
+}
+
---
+++ login ine/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using MySql.Data.MySqlClient; // Biblioteca para trabajar con MySQL
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login_ine
+{
+    public partial class Form1 : Form
+    {
+        // Cadena de conexión: contiene la información para conectar a la base de datos MySQL.
+        // Server: Dirección del servidor (en este caso, localhost para tu máquina local).
+        // Port: Puerto en el que MySQL escucha (3306 por defecto).
+        // Database: Nombre de la base de datos que estás usando (ine_inventario).
+        // User Id: Usuario para la conexión (root).
+        // Password: Contraseña del usuario.
+        string cadenaConexion = "Server=localhost;Port=3306;Database=ine_inventario;User Id=root;Password=21082007jd;";
+
+        public Form1()
+        {
+            InitializeComponent(); // Inicializa los componentes del formulario.
+        }
+        // Evento asociado al clic en el label (o botón) para iniciar sesión.   
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // Se obtiene el nombre de usuario y la contraseña ingresados por el usuario.
+            string usuario = txtadmin.Text;
+            string contrasena = txtContraseña.Text;
+
+            // Llamamos a la función para autenticar al usuario.
+            if (AutenticarUsuario(usuario, contrasena))
+            {
+                // Si la autenticación es exitosa, mostramos un mensaje.
+                MessageBox.Show("Inicio de sesión exitoso", "Login");
+            }
+            else
+            {
+                // Si falla la autenticación, mostramos un mensaje de error.
+                MessageBox.Show("Usuario o contraseña incorrectos", "Error");
+            }
+        }
+
+        // Método para autenticar al usuario en la base de datos.
+        // Recibe el nombre de usuario y la contraseña ingresados por el usuario.
+        private bool AutenticarUsuario(string usuario, string contrasena)
+        {
+            bool resultado = false; // Variable para almacenar el resultado de la autenticación.
+
+            // Consulta SQL para verificar si existe un usuario con las credenciales ingresadas.
+            string query = "SELECT COUNT(*) FROM usuarios WHERE NombreUsuario = @Usuario AND Contrasena = @Contraseña";
+
+
+            // Bloque para manejar la conexión a la base de datos.
+            using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open(); // Abre la conexión con la base de datos.
+
+                    // Preparamos el comando SQL con la consulta y los parámetros.
+                    using (MySqlCommand comando = new MySqlCommand(query, conexion))
+                    {
+                        // Agregamos los valores para los parámetros de la consulta.
+                        comando.Parameters.AddWithValue("@Usuario", usuario);
+                        comando.Parameters.AddWithValue("@Contraseña", contrasena);
+
+                        // Ejecutamos la consulta y obtenemos el resultado.
+                        int count = Convert.ToInt32(comando.ExecuteScalar());
+
+                        // Si el resultado es mayor a 0, significa que las credenciales son válidas.
+                        resultado = count > 0;
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    // Capturamos errores específicos de MySQL y los mostramos.
+                    MessageBox.Show("Error de base de datos: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    // Capturamos cualquier otro tipo de error y lo mostramos.
+                    MessageBox.Show("Error general: " + ex.Message);
+                }
+            }
+
+            // Retornamos el resultado de la autenticación (true si es válido, false si no lo es).
+            return resultado;
+        }
+
+       
+    }
+}
